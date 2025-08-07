0ae0a95963f6b939727b8fc37266af2e257d5ca1
farmaciasbomlar <bomlarcomercial@gmail.com>
2025-08-07 11:32:48 -0300

    Corrige main.py para execução no Render

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ class Item(BaseModel):
 @app.post("/scraper")
 async def scrape(item: Item):
     return {"message": f"Hello {item.name}, scraping done!"}
+
+@app.get("/")
+def read_root():
+    return {"message": "Está funcionando!"}
+
+# Adicione isso no final:
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
